f61aa37bba67bf742717513e076de87a3d9a5f25
Andrey <andreyofbbrasil@gmail.com>
2026-04-18 15:24:52 -0300

    feat: Add Book entity with ManyToMany relationship to Author

---
+++ src/main/java/com/andrey/librarymanager/model/Book.java
@@ -0,0 +1,53 @@
+package com.andrey.librarymanager.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String title;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    @NotNull
+    @PastOrPresent
+    @Column(nullable = false)
+    private Integer publicatyonYear;
+
+    @Positive
+    @Column(nullable = false)
+    private Integer totalCopies;
+
+    @Column(nullable = false)
+    private Integer availableCopies;
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_author",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
+    private Set<Author> authors = new HashSet<>();
+}
