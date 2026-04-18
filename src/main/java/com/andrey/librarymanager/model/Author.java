673850cbbe7d795ba7fa7e2a7495b21492248b49
Andrey <andreyofbbrasil@gmail.com>
2026-04-18 14:46:35 -0300

    feat: add Author entity

---
+++ src/main/java/com/andrey/librarymanager/model/Author.java
@@ -0,0 +1,36 @@
+package com.andrey.librarymanager.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "authors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String nationality;
+
+    @NotNull
+    @Past
+    @Column(nullable = false)
+    private LocalDate birthDate;
+}
