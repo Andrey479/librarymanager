91e385eab736b3a5729a4daba4232f74130d0d93
Andrey <andreyofbbrasil@gmail.com>
2026-05-31 16:18:59 -0300

    fix(validation): remove @PastOrPresent constraint from publicationYear
    Allow future publication years to be accepted in Book entity

--- src/main/java/com/andrey/librarymanager/model/Book.java
+++ src/main/java/com/andrey/librarymanager/model/Book.java
@@ -3,7 +3,6 @@ package com.andrey.librarymanager.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
 
@@ -32,7 +31,6 @@ public class Book {
     private String isbn;
 
     @NotNull
-    @PastOrPresent
     @Column(nullable = false)
     private Integer publicationYear;
 
